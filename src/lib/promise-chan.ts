fb0dc4293c043722336bf01410ebe8d2e7c7527a
hankei6km <gh-hankei6km@outlook.jp>
2022-02-06 02:15:55 +0900

    Update comment

--- src/lib/promise-chan.ts
+++ src/lib/promise-chan.ts
@@ -61,11 +61,14 @@ export class Make<T> {
     return this.writeFunc(p)
   }
   private async reciver(): Promise<{ value: T | undefined; done: boolean }> {
+    // バッファーが埋まっていない場合は、write されるまで待つ.
+    // close されていれば素通し.
     while (this.buf.length < this.bufSize && !this.closed) {
-      // バッファーが埋まっていない場合は、write されるまで待つ.
       await this.bufPromise
       this.bufReset()
     }
+    // バッファーを消費していたら終了.
+    // 通常は消費しない、close されていれば何度か呼びだされるうちに消費される.
     if (this.buf.length > 0) {
       const pa = this.buf.map(
         (b, i) =>
