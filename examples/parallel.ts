446d96b0951492ad0fcd267be8115b5c899b6136
hankei6km <gh-hankei6km@outlook.jp>
2022-02-07 13:01:17 +0900

    ** 実行結果を更新

--- examples/parallel.ts
+++ examples/parallel.ts
@@ -3,7 +3,7 @@ import { Chan } from '../src/index.js'
 const s: [string, number][] = [
   ['0', 100],
   ['1', 280],
-  ['2', 10],
+  ['2', 50],
   ['3', 80],
   ['4', 200],
   ['5', 240],
@@ -44,21 +44,21 @@ const n = Date.now()
 
 // $ node --loader ts-node/esm examples/serial.ts
 //
-// write elapsed time: 0101 index: 0
-// read  elapsed time: 0102 value: 0
-// write elapsed time: 0149 index: 1
-// read  elapsed time: 0150 value: 1
-// write elapsed time: 0330 index: 2
-// read  elapsed time: 0330 value: 2
-// write elapsed time: 0410 index: 3
-// read  elapsed time: 0411 value: 3
-// write elapsed time: 0610 index: 4
-// read  elapsed time: 0611 value: 4
-// write elapsed time: 0850 index: 5
-// read  elapsed time: 0850 value: 5
-// write elapsed time: 1000 index: 6
-// read  elapsed time: 1001 value: 6
-// write elapsed time: 1010 index: 7
-// read  elapsed time: 1011 value: 7
-// write elapsed time: 1140 index: 8
-// read  elapsed time: 1140 value: 8
+// write elapsed time: 0000 index: 0
+// write elapsed time: 0004 index: 1
+// write elapsed time: 0007 index: 2
+// write elapsed time: 0051 index: 3
+// read  elapsed time: 0051 value: 2
+// write elapsed time: 0080 index: 4
+// read  elapsed time: 0081 value: 3
+// write elapsed time: 0099 index: 5
+// read  elapsed time: 0100 value: 0
+// write elapsed time: 0200 index: 6
+// read  elapsed time: 0201 value: 4
+// write elapsed time: 0201 index: 7
+// read  elapsed time: 0201 value: 6
+// write elapsed time: 0202 index: 8
+// read  elapsed time: 0202 value: 7
+// read  elapsed time: 0202 value: 8
+// read  elapsed time: 0240 value: 5
+// read  elapsed time: 0280 value: 1
